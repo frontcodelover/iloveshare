4d00fda2ddf9beea2743fcbfbf667c63b7932238
frontcodelover <contact@avantjetaisriche.com>
2022-07-02 11:51:51 +0200

    ILS-11 [TEST] Google provider and auth()

--- frontend/components/login.js
+++ frontend/components/login.js
@@ -1,94 +1,71 @@
-import React, { useEffect, useState } from 'react';
-
+import React, { useEffect, useState } from "react";
 
 const backendUrl = process.env.NEXT_PUBLIC_API_URL;
 
-const providersNames = [
-    'discord',
-    'facebook',
-    'github',
-    'google',
-    'instagram',
-    'linkedin',
-    'reddit',
-    'twitch',
-    'twitter',
-    'vk',
-    'auth0',
-];
+const providersNames = ["github", "google", "twitter", "auth0"];
 
-
-
-const LoginButton = (props) => <a href={`${backendUrl}/api/connect/${props.providerName}`}>
-    <button style={{ width: '150px' }}>Connect to {props.providerName}</button>
-  </a>;
+const LoginButton = (props) => (
+  <a href={`${backendUrl}/api/connect/${props.providerName}`}>
+    <button style={{ width: "150px" }}>Connect to {props.providerName}</button>
+  </a>
+);
 
 const LogoutButton = (props) => <button onClick={props.onClick}>Logout</button>;
 
-const Login = (props) => { 
+const Login = () => {
+  const [isLogged, setIsLogged] = useState(false);
+  const [userName, setUserName] = useState(null);
+  const [dataJwt, setDataJwt] = useState("");
 
-    const [isLogged, setIsLogged] = useState(false)
-    const [dataName, setDataName] = useState("coucou")
-    const [userName, setUserName] = useState(null)
-    const [dataJwt, setDataJwt] = useState("")
-    
-    useEffect(() => {
-        let data = !localStorage.getItem('jwt')
-        console.log(localStorage)
-        if (!data) {
-            setDataJwt(localStorage.removeItem('jwt'))
-            setIsLogged(false)
-            
-        } else {
-            setDataJwt(localStorage.getItem('jwt'))
-            setDataName("Coucou")
-            setUserName(localStorage.getItem('name'))
-            setIsLogged(true)
-        }
-
-    }, [])
-    
-    
-
-      
-        const logout = (e) => {
-            e.preventDefault();
-
-        setIsLogged(false);
-      };
-
-        
-    
-    
-
-let buttons;
-
-if (isLogged) {
-    buttons = <LogoutButton onClick={logout} />;
-} else {
-    buttons = <ul style={{ listStyleType: 'none' }}>
-      {providersNames.map((providerName, i) => <li key={providerName}>
-        <LoginButton providerName={providerName}/>
-        </li>)}
-    </ul>;
-  }
-  
-  let text;
-  
-  if (isLogged) {
-      
-          text = `Welcome ${userName}, you are connected!`;
-   
+  useEffect(() => {
+    let data = !localStorage.getItem("jwt");
+    console.log(localStorage);
+    if (!data) {
+      setIsLogged(false);
     } else {
-        text = `You are not connected. Please log in. `;
+      setDataJwt(localStorage.getItem("jwt"));
+      setUserName(localStorage.getItem("username"));
+      setIsLogged(true);
     }
-    
-    
-    
-    return <div>
-    <p>{text}</p>
-    {buttons}
-  </div>;
-}
+  }, []);
+
+  const logout = (e) => {
+    e.preventDefault();
+    setDataJwt(localStorage.removeItem("jwt"));
+    setUserName(localStorage.removeItem("username"));
+    setIsLogged(false);
+  };
+
+  let buttons;
+
+  if (isLogged) {
+    buttons = <LogoutButton onClick={logout} />;
+  } else {
+    buttons = (
+      <ul style={{ listStyleType: "none" }}>
+        {providersNames.map((providerName, i) => (
+          <li key={providerName}>
+            <LoginButton providerName={providerName} />
+          </li>
+        ))}
+      </ul>
+    );
+  }
+
+  let text;
+
+  if (isLogged) {
+    text = `Welcome ${userName}, you are connected!`;
+  } else {
+    text = `You are not connected. Please log in. `;
+  }
+
+  return (
+    <div>
+      <p>{text}</p>
+      {buttons}
+    </div>
+  );
+};
 
 export default Login;
