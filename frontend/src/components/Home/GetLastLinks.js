4272d38c30313f205e407f45e9fb1ec9250859e8
frontcodelover <contact@avantjetaisriche.com>
2022-07-12 01:41:04 +0200

    ILS-7 [REFACTO] custom hook for fetch
    named : useFetchData

--- frontend/src/components/Home/GetLastLinks.js
+++ frontend/src/components/Home/GetLastLinks.js
@@ -1,68 +1,37 @@
-import React, {useEffect, useState} from 'react'
+import React from "react";
+import { useFetchData } from "../Hooks/useFetchData";
+
+const backendUrl = process.env.REACT_APP_API_URL;
 
 export default function GetLastLinks() {
+  const {
+    data: lastLinks,
+    isLoading,
+    error,
+  } = useFetchData(`${backendUrl}/api/links?populate=*`);
 
-  const [links, setLinks] = useState([]);
-  const [loading, setLoading] = useState(true);
- 
-  const backendUrl = process.env.REACT_APP_API_URL;
-
-  useEffect(() => {
-    let shouldCancel = false;
-    async function fetchData() {
-      try {
-        const res = await fetch(`${backendUrl}/api/links?populate=*`);
-        if (res.ok) {
-          const data = await res.json();
-          if (!shouldCancel) {
-            setLinks(data.data);
-          }
-        } else {
-          throw new Error('Something went wrong');
-        }
-      }
-      catch (err) {
-        console.log("Tu as une erreur", err);
-      } finally {
-        setLoading(false);
-      }
-    }
-    fetchData();
-    return () => {
-      shouldCancel = true;
-    }
-  }, [backendUrl]);
-
-    
-    const randomNumberForKey = Math.floor(Math.random() * links.length);
-    
+  const randomNumberForKey = Math.floor(Math.random() * lastLinks.length);
 
   return (
     <div>
-      {console.log(links)}
-      {loading ? <p>Loading...</p> : 
-      links.map(link => {
-        return (
-          <div key={randomNumberForKey}>
-            <h1>{link.attributes.name}</h1>
-            <p>{link.id}</p>
-            <p>{link.attributes.url}</p>
-            <p>{link.attributes.type}</p>
-            {link.attributes.tag.map(tag => {
-              return (
-  
-                  <p>{tag.name}</p>
-
-              )
-            })}
-          </div>
-   
-        )
-      }
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        lastLinks.map((link) => {
+          return (
+            <div key={randomNumberForKey}>
+              <h1>{link.attributes.name}</h1>
+              <p>{link.id}</p>
+              <p>{link.attributes.url}</p>
+              <p>{link.attributes.type}</p>
+              {link.attributes.tag.map((tag) => {
+                return <p>{tag.name}</p>;
+              })}
+            </div>
+          );
+        })
       )}
-    
-
-    
-      </div>
-  )
+      {error && <p>{error}</p>}
+    </div>
+  );
 }
--- frontend/src/components/Hooks/useFetchDataByTags.js
+++ frontend/src/components/Hooks/useFetchDataByTags.js
@@ -3,35 +3,34 @@ import { useParams } from "react-router-dom";
 
 export default function useFetchDataByTags() {
   let { tags } = useParams();
-  
+
   const [dataResFilterByTags, setDataResFilterByTags] = useState([]);
   const backendUrl = process.env.REACT_APP_API_URL;
 
   useEffect(() => {
     let shouldCancel = false;
     async function fetchData() {
-
       try {
-        const res = await fetch(`${backendUrl}/api/links?populate=*&filters[tag][name]=${tags}`)
+        const res = await fetch(
+          `${backendUrl}/api/links?populate=*&filters[tag][name]=${tags}`
+        );
         if (res.ok) {
           const data = await res.json();
           if (!shouldCancel) {
             setDataResFilterByTags(data.data);
           }
         } else {
-          throw new Error('Something went wrong');
-        } 
+          throw new Error("Something went wrong");
+        }
       } catch (err) {
         console.log("Tu as une erreur", err);
-      } 
+      }
     }
     fetchData();
     return () => {
       shouldCancel = true;
-    }
+    };
   }, [backendUrl, tags]);
 
-   
-
-  return (dataResFilterByTags);
+  return dataResFilterByTags;
 }
